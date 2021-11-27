2aa10bbcdcf7f614e1919a3e7c241d19229952d5
WbDmitry <wb.dmitry@yandex.ru>
2021-11-27 22:50:09 +0300

    Добавил мини логирование :)

--- app/src/main/java/com/wbdmitry/moviesearch/MainActivity.kt
+++ app/src/main/java/com/wbdmitry/moviesearch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wbdmitry.moviesearch
 
+import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Bundle
@@ -7,9 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.wbdmitry.moviesearch.databinding.ActivityMainBinding
 import com.wbdmitry.moviesearch.ui.main.movielist.MovieListFragment
 
+const val NAME_LOG_EVENT = "logEvent"
+const val NAME_EVENT = "event"
+const val NUMBER = 0
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val receiver = MainBroadcastReceiver()
+    private val intentMyIntentService by lazy { Intent(this, MyIntentService::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +31,21 @@ class MainActivity : AppCompatActivity() {
                 ).commit()
         }
         registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        startEventLog(intentMyIntentService, getString(R.string.user_logged))
+    }
+
+    private fun startEventLog(intent: Intent, event: String) {
+        startService(
+            intent.putExtra(NAME_EVENT, NUMBER).putExtra(
+                NAME_LOG_EVENT,
+                event
+            )
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+        startEventLog(intentMyIntentService, getString(R.string.user_exit))
     }
 }
